82edb885d6c7fa8a05ab24b2a547e2f2a87e3c9d
VinayKovvuri28 <vinaykovvuri28@gmail.com>
2024-01-02 20:58:10 +0530

    ok

--- JS/Projects/4  Myntra Functional Clone/scripts/index.js
+++ JS/Projects/4  Myntra Functional Clone/scripts/index.js
@@ -1,52 +1,57 @@
 let bagItems;
-onLoad();
+onLoad()
 
 function onLoad() {
-  let bagItemsStr = localStorage.getItem('bagItems');
-  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
-  displayItemsOnHomePage();
-  displayBagIcon();
-}
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+    displayItemsOnHomePage();
+    displayBagIcon();
+};
 
-function addToBag(itemId) {
-  bagItems.push(itemId);
-  localStorage.setItem('bagItems', JSON.stringify(bagItems));
-  displayBagIcon();
-}
+function addToBag(productId) {
+    bagItems.push(productId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    displayBagIcon();
+};
 
 function displayBagIcon() {
-  let bagItemCountElement = document.querySelector('.bag-item-count');
-  if (bagItems.length > 0) {
-    console.log('I am here');
-    bagItemCountElement.style.visibility = 'visible';
-    bagItemCountElement.innerText = bagItems.length;
-  } else {
-    bagItemCountElement.style.visibility = 'hidden';
-  }
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if (bagItems.length > 0) {
+        console.log('I am here');
+        bagItemCountElement.style.visibility = 'visible';
+        bagItemCountElement.innerHTML = bagItems.length;
+    }
+    else {
+        bagItemCountElement.style.visibility = 'hidden';
+    }
 }
 
 function displayItemsOnHomePage() {
-  let itemsContainerElement = document.querySelector('.items-container');
-  if (!itemsContainerElement) {
-    return;
-  }
-  let innerHtml = '';
-  items.forEach(item => {
-    innerHtml += `
-    <div class="item-container">
-      <img class="item-image" src="${item.image}" alt="item image">
-      <div class="rating">
-          ${item.rating.stars} ⭐ | ${item.rating.count}
-      </div>
-      <div class="company-name">${item.company}</div>
-      <div class="item-name">${item.item_name}</div>
-      <div class="price">
-          <span class="current-price">Rs ${item.current_price}</span>
-          <span class="original-price">Rs ${item.original_price}</span>
-          <span class="discount">(${item.discount_percentage}% OFF)</span>
-      </div>
-      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
-    </div>`
-  });
-  itemsContainerElement.innerHTML = innerHtml;
-}
+    let itemsContainerElement = document.querySelector('.items-container');
+    if (!itemsContainerElement) {
+        return;
+    }
+    let innerHTML = '';
+    items.forEach(product => {
+        innerHTML += `
+        <div class="item-container">
+            <img class="item-image" src="${product.image}" alt="item image">
+            <div class="rating">
+                ${product.rating.stars} ⭐ | ${product.rating.count}
+            </div>
+            <div class="company-name">
+                ${product.company}
+            </div>
+            <div class="item-name">
+                ${product.item_name}
+            </div>
+            <div class="product-price">
+                <span class="product-discountedPrice">Rs ${product.current_price}</span>
+                <span class="product-strike">Rs ${product.original_price}</span>
+                <span class="product-discountPercentage">(${product.discount_percentage}% OFF)</span>
+            </div>
+            <button type="button" class="btn btn-success btn-add-bag" onclick="addToBag(${product.id})">Add to Bag</button>
+        </div>`
+    });
+    itemsContainerElement.innerHTML = innerHTML;
+};
